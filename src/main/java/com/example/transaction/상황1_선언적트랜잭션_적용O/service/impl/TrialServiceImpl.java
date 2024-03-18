1ba40a7700fcd5eaaab1d19a93f19d91ef062e5b
jkkim <jkkim@interfaceit.co.kr>
2024-03-18 18:05:29 +0900

    상황1 재수정
    분리의 뜻에 대해서
    내부트랜잭션을 REQUIRED_NEW 하는것은 내부의 결과가 외부에 영향을 주지않지만
    진정한 분리는 2개의 트랜잭션이 서로 관련이 없게 만드는 것 외부트랜잭션의 결과 순서에따라 내부트랜잭션의 결과에 영향을 줄수없도록 수정

--- src/main/java/com/example/transaction/상황1_선언적트랜잭션_적용O/service/impl/TrialServiceImpl.java
+++ src/main/java/com/example/transaction/상황1_선언적트랜잭션_적용O/service/impl/TrialServiceImpl.java
@@ -6,6 +6,8 @@ import com.example.transaction.상황1_선언적트랜잭션_적용O.service.App
 import com.example.transaction.상황1_선언적트랜잭션_적용O.vo.TrialVO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,15 +18,25 @@ public class TrialServiceImpl implements TrialService {
 
     @Override
     public TrialVO startTrial(TrialVO param) {
-        trialDao.create(param);
+        createTrial(param);
         approvalService.approve(param.getApprovalVO());
         return param;
     }
 
     @Override
     public TrialVO updateTrialInfo(TrialVO param, Integer idx) {
-        trialDao.update(param);
+        updateTrial(param);
         approvalService.approve(param.getApprovalVO());
         return param;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void createTrial(TrialVO param){
+        trialDao.create(param);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void updateTrial(TrialVO param){
+        trialDao.update(param);
     }
 }
