5e45ba206ceb5d56c2a8a025772fd817a5f76203
egjeong <egjeong@interfaceit.co.kr>
2024-03-19 09:12:37 +0900

    문제 추가

---
+++ src/main/java/com/example/transaction/상황5_Transactional/dao/TrialDao.java
@@ -0,0 +1,24 @@
+package com.example.transaction.상황5_Transactional.dao;
+
+import com.example.transaction.상황5_Transactional.vo.TrialVO;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.SelectKey;
+import org.apache.ibatis.annotations.Update;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TrialDao {
+
+    @SelectKey(
+            statement = "SELECT NVL(MAX(IDX), 0) + 1 FROM TRIAL",
+            keyProperty = "idx",
+            before = true,
+            resultType = Integer.class
+    )
+    @Insert("INSERT INTO TRIAL (IDX, ...) VALUES (#{idx}, ...)")
+    int create(TrialVO vo);
+
+    @Update("UPDATE TRIAL SET ... WHERE IDX = #{idx}")
+    int update(TrialVO vo);
+
+}
