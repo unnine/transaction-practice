d0e572c8ef57f40ac0810ec363472c60afd04e4e
neso4 <67271661+neso4@users.noreply.github.com>
2024-03-25 23:05:13 +0900

    추가: 문제 2

--- src/main/java/com/example/transaction/상황2_선언적트랜잭션_적용X/service/impl/ApprovalServiceImpl.java
+++ src/main/java/com/example/transaction/상황2_선언적트랜잭션_적용X/service/impl/ApprovalServiceImpl.java
@@ -4,16 +4,28 @@ import com.example.transaction.상황2_선언적트랜잭션_적용X.dao.Approva
 import com.example.transaction.상황2_선언적트랜잭션_적용X.service.ApprovalService;
 import com.example.transaction.상황2_선언적트랜잭션_적용X.vo.ApprovalVO;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ApprovalServiceImpl implements ApprovalService {
 
     private final ApprovalDao approvalDao;
+    private PlatformTransactionManager txManager;
 
     @Override
     public void approve(ApprovalVO vo) {
-        approvalDao.create(vo);
+        TransactionStatus status = txManager.getTransaction(new DefaultTransactionDefinition());
+        try {
+            approvalDao.create(vo);
+        } catch (RuntimeException e) {
+            txManager.rollback(status);
+        }
+        txManager.commit(status);
     }
 }
--- src/main/java/com/example/transaction/상황2_선언적트랜잭션_적용X/service/impl/TrialServiceImpl.java
+++ src/main/java/com/example/transaction/상황2_선언적트랜잭션_적용X/service/impl/TrialServiceImpl.java
@@ -5,6 +5,9 @@ import com.example.transaction.상황2_선언적트랜잭션_적용X.service.Tri
 import com.example.transaction.상황2_선언적트랜잭션_적용X.vo.TrialVO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 @Service
 @RequiredArgsConstructor
@@ -12,18 +15,43 @@ public class TrialServiceImpl implements TrialService {
 
     private final TrialDao trialDao;
     private final ApprovalServiceImpl approvalService;
+    private final PlatformTransactionManager txManager;
 
     @Override
     public TrialVO startTrial(TrialVO param) {
-        trialDao.create(param);
-        approvalService.approve(param.getApprovalVO());
+        create(param);
+        approve(param);
         return param;
     }
 
     @Override
     public TrialVO updateTrialInfo(TrialVO param, Integer idx) {
-        trialDao.update(param);
-        approvalService.approve(param.getApprovalVO());
+        update(param);
+        approve(param);
         return param;
+    }
+
+    private void create(TrialVO param) {
+        TransactionStatus status = txManager.getTransaction(new DefaultTransactionDefinition());
+        try {
+            trialDao.create(param);
+        } catch (RuntimeException e) {
+            txManager.rollback(status);
+        }
+        txManager.commit(status);
+    }
+
+    private void update(TrialVO param) {
+        TransactionStatus status = txManager.getTransaction(new DefaultTransactionDefinition());
+        try {
+            trialDao.create(param);
+        } catch (RuntimeException e) {
+            txManager.rollback(status);
+        }
+        txManager.commit(status);
+    }
+
+    private void approve(TrialVO param) {
+        approvalService.approve(param.getApprovalVO());
     }
 }
