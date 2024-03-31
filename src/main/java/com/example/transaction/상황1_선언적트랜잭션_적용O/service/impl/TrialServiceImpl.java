241deb5c240da7b9e2903c62d3e147fac43571ad
neso4 <67271661+neso4@users.noreply.github.com>
2024-03-31 20:39:51 +0900

    수정: 문제 1

--- src/main/java/com/example/transaction/상황1_선언적트랜잭션_적용O/service/impl/TrialServiceImpl.java
+++ src/main/java/com/example/transaction/상황1_선언적트랜잭션_적용O/service/impl/TrialServiceImpl.java
@@ -6,7 +6,6 @@ import com.example.transaction.상황1_선언적트랜잭션_적용O.service.App
 import com.example.transaction.상황1_선언적트랜잭션_적용O.vo.TrialVO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,27 +16,13 @@ public class TrialServiceImpl implements TrialService {
 
     @Override
     public TrialVO startTrial(TrialVO param) {
-        createTrial(param);
-        approve(param);
+        trialDao.create(param);
         return param;
     }
 
     @Override
     public TrialVO updateTrialInfo(TrialVO param, Integer idx) {
-        createTrial(param);
-        approve(param);
-        return param;
-    }
-
-    private void approve(TrialVO param) {
-        approvalService.approve(param.getApprovalVO());
-    }
-
-    private void createTrial(TrialVO param) {
-        trialDao.create(param);
-    }
-
-    private void update(TrialVO param) {
         trialDao.update(param);
+        return param;
     }
 }
